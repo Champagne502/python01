fc75dd7476018c9c67b20c500801826f04c7fc5d
Champagne <50206@cru.ac.th>
2022-07-04 11:34:01 +0700

    first commit

---
+++ Downloads/Champagne611/work1.py
@@ -0,0 +1,11 @@
+print("hollo 6/11")
+print("Nuttaya Sompan")
+print("No.37")
+print("อายุ :",17, "ปี")
+print("my brithday:",20050102)
+print("เรียนที่โรงเรียนชลราษฎรอำรุง")
+print("Grade12")
+print("Hobby:",watch a movie)
+print("My fav subject is Math")
+print("My phone is 0946521379")
+#Nuttaya Sompan M.6/11 No.37
---
+++ Downloads/Champagne611/work2.py
@@ -0,0 +1,21 @@
+hello="hello M.6/11"
+print(hello)
+name = "Nuttaya Sompan"
+print(name)
+nickname= "Champagne"
+print(nickname)
+birthday="2005/01/02"
+print(birthday)
+phone="0946521379"
+print(phone)
+hobby= "watch a movie"
+print(hobby)
+school= "chonratsadornaumrung"
+print(school)
+favorite="food"
+print(favorite)
+age= "17"
+print(age)
+family="father mother brother me"
+print(family)
+#Nuttaya Sompan  M.6/11 No.37
